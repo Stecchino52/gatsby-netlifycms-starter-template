9be9415d0091b4241aac131684eea70979fed923
Stecchino52 <113646946+Stecchino52@users.noreply.github.com>
2022-09-27 18:06:53 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import GitHubButton from 'react-github-btn'
+import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
